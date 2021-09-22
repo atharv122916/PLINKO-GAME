69ed1a0454a169f1ecff7ddb33b0f384e00806c7
atharv122916 <83113926+atharv122916@users.noreply.github.com>
2021-09-22 13:04:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -61,14 +61,14 @@ function draw() {
 
   //create the particles using frameCount
   if (frameCount %60 === 0) {
-    particles.push(new Particle(random(0,800),0));  
+    particles.push(new Particles(random(0,800),0));  
   }
 
 
   //display the particles 
 
   for (var k = 0; k<particles.length; k++) {
-    particles[h].display();
+    particles[k].display();
   }
 
   for (var j = 0; j<plinkos.length; j++) {
